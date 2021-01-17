cc97675a1fa7819a99a8c2360fac27c414230f32
occ42 <shizu.hama3013@gmail.com>
2021-01-17 21:36:23 +0900

    Stop putting comments in front of new lines

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/dein.toml ~/.config/nvim/dein.toml
 ln -sf ~/dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
 ln -sf ~/dotfiles/ftplugin ~/.config/nvim/ftplugin
+ln -sf ~/dotfiles/after ~/.config/nvim/after
